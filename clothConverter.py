3dfd7a0f54fd57fa3f1d60cb9af0a968cd7b5382
Rae Fu <rae.fu22@gmail.com>
2024-02-19 15:14:32 -0500

    Multiple curves as folds

--- clothConverter.py
+++ clothConverter.py
@@ -39,9 +39,7 @@ cmds.separator(height = 10)
 cmds.radioButtonGrp('clothShape', label='Cloth Shape ', labelArray2=['Elliptic', 'Rectangular'], numberOfRadioButtons=2)
 cmds.floatSliderGrp('width', label='Width ', field = True, min = 1, max = 40, v = 5)
 cmds.floatSliderGrp('height', label='Height ', field = True, min = 1, max = 40, v = 5)
-useCrease = cmds.checkBoxGrp('useCrease', numberOfCheckBoxes=1, label='Use Selected as Crease ')
-
-#change all creases to folds
+useFolds = cmds.checkBoxGrp('useFolds', numberOfCheckBoxes=1, label='Use Selected as Fold(s) ')
 
 useTable = cmds.checkBoxGrp('useTable', numberOfCheckBoxes=1, label='Use Selected as Table ', onc = hideTableOp, ofc = showTableOp)
 tableScale = cmds.floatSliderGrp('tableScale', label='Table Scale ', field = True, min = 1, max = 40, v = 3)
@@ -67,6 +65,9 @@ def convertCloth():
     clothShape = cmds.radioButtonGrp('clothShape', q = True, sl = True)
     width = cmds.floatSliderGrp('width', q = True, v = True)
     height = cmds.floatSliderGrp('height', q = True, v = True)
+    useTable = cmds.checkBoxGrp('useTable', q = True, v1=True)
+    useFolds = cmds.checkBoxGrp('useFolds', q = True, v1=True)
+        
     if (isTablecloth):
         useTable = cmds.checkBoxGrp('useTable', q = True, v1=True)
         tableScale = cmds.floatSliderGrp('tableScale', q = True, v = True)
@@ -82,86 +83,29 @@ def convertCloth():
             cmds.select(name + 'collider')
             cmds.scale(tableScale, tableScale, tableScale, relative = True)
     
-        clothmesh = cmds.polyPlane(w=width, h=width, sx=10, sy=10, ax=[0, 1, 0], cuv=2, ch=1, n= name + 'clothmesh')
-        if(clothShape == 1):    
-            clothmesh = cmds.polyCircularize(name + 'clothmesh')
-        cmds.select(name + 'clothmesh')
-        ratio = height/width
-        clothmesh = cmds.scale(ratio, 1, 1, relative = True)
-        
-        #location
-        clothLocation = 0, 2, 0
-        if (useTable):
-            clothLocation = cmds.objectCenter(collider)
-            clothLocation[1] += 2
-        cmds.move(clothLocation[0], clothLocation[1], clothLocation[2], r=True)
-        
-        #add subdivisions to cloth mesh
-        clothSubdivide = 2
-        if (width > 7 or height >  7):
-            clothSubdivide = 3
-        print(clothSubdivide)
-        clothmesh = cmds.polySmooth(name + 'clothmesh', dv = clothSubdivide, mth=0, sl = 2)
-        
-        #convert to nCloth
-        outMesh = cmds.createNode('nRigid', name=name + 'nRigid1')
-        
-        cmds.connectAttr(name + 'collider' + 'Shape.worldMesh[0]', name + 'nRigid1.inputMesh')
-        cmds.createNode('nucleus', name= name + 'nucleus1')
-        cmds.connectAttr(name + 'nRigid1.currentState', name + 'nucleus1.inputPassive[0]')
-        cmds.connectAttr(name + 'nRigid1.startState', name + 'nucleus1.inputPassiveStart[0]')
-        cmds.connectAttr(name + 'nucleus1.startFrame', name + 'nRigid1.startFrame')
-        
-        cmds.createNode('nCloth', name= name + 'nCloth1')
-        cmds.connectAttr(name + 'nucleus1.outputObjects[0]', name + 'nCloth1.nextState')
-        cmds.connectAttr(name + 'clothmeshShape.worldMesh[0]', name + 'nCloth1.inputMesh')
-        cmds.connectAttr(name + 'nucleus1.startFrame', name + 'nCloth1.startFrame')
-        cmds.connectAttr(name + 'nCloth1.startState', name + 'nucleus1.inputActiveStart[0]')
-        cmds.connectAttr(name + 'nCloth1.currentState', name + 'nucleus1.inputActive[0]')
-        outMesh = cmds.createNode('mesh', n = name + 'cloth1')
-        cmds.connectAttr(name + 'nCloth1.outputMesh', name + 'cloth1.inMesh')
-        cmds.setAttr(name + 'nRigid1.thickness', 0.0)
-        cmds.setAttr(name + 'nCloth1.thickness', 0.02)
-        
-        time_node = cmds.createNode('time', n = 'time')
-        cmds.playbackOptions(minTime=1, maxTime=300, animationStartTime=1, animationEndTime=300)
-        cmds.currentTime( query=True )
-        
-        cmds.connectAttr('time.outTime', name + 'nCloth1.currentTime')
-        cmds.connectAttr('time.outTime', name + 'nucleus1.currentTime')
-        cmds.connectAttr('time.outTime', name + 'nRigid1.currentTime')
-        
-        cmds.expression(s=f"{time_node}.outTime = `currentTime -q`;")
-       
-        #nCloth attribute adjustments
-        cmds.setAttr(name + 'nCloth1.lift', 0)
-        cmds.setAttr(name + 'nCloth1.friction', 0.5)   
-        cmds.setAttr(name + 'nCloth1.stickiness', 0.2)
-        cmds.setAttr(name + 'nCloth1.stretchResistance', 120)
-        
-        shader = cmds.shadingNode('aiStandardSurface', asShader = True, n=name + 'shader')
-                    
-        cmds.sets(renderable=True, noSurfaceShader= True, empty=True, n= 'aiSurfaceShader' + name + 'SG')
-        cmds.select(outMesh)
-        cmds.hyperShade(assign = 'aiSurfaceShader' + name + 'SG')
-        cmds.connectAttr(name + 'shader.outColor', 'aiSurfaceShader' + name +'SG.surfaceShader', f=True)
         
     else:
-        useCrease = cmds.checkBoxGrp('useCrease', q = True, v1=True)
         
-        if (useCrease == True):
+        if (useFolds == True):
             curveObj = cmds.ls(selection = True)
-            curveName = curveObj[0]
-            
-            sweepNode = cmds.sweepMeshFromCurve(oneNodePerCurve=0)
-            cmds.rename(sweepNode, f'sweepMesh{name}')
-            
-            cmds.setAttr(f'sweepMesh{name}.scaleProfileX', 0.197368)
-            connections = cmds.listConnections(f'sweepMesh{name}', source = False)
-            cmds.rename(connections[0], name + 'creaseCollider')
-            cmds.select(name + 'creaseCollider')
-            cmds.polySmooth(mth=0, sdt=2, ovb=1, ofb=3, ofc=0, ost=0, ocr=0, dv=2, bnr=1, c=1, kb=1, ksb=1, khe=0, kt=1, kmb=1, suv=1, peh=0, sl=1, dpe=1, ps=0.1, ro=1, ch=1)
-            
+            mergeList = []
+            for curveNum in range(len(curveObj)):
+                curveName = curveObj[curveNum]
+                cmds.select(curveName)
+                curveNumName = curveNum + 1
+                sweepNode = cmds.sweepMeshFromCurve(oneNodePerCurve=1)
+                cmds.rename(sweepNode, f'sweepMesh{name}_{curveNumName}')
+                cmds.setAttr(f'sweepMesh{name}_{curveNumName}.scaleProfileX', 0.197368)
+                connections = cmds.listConnections(f'sweepMesh{name}_{curveNumName}', source = False)
+                cmds.rename(connections[0], name + f'foldsMesh{curveNumName}')
+                cmds.select(name + f'foldsMesh{curveNumName}')
+                cmds.polySmooth(mth=0, sdt=2, ovb=1, ofb=3, ofc=0, ost=0, ocr=0, dv=2, bnr=1, c=1, kb=1, ksb=1, khe=0, kt=1, kmb=1, suv=1, peh=0, sl=1, dpe=1, ps=0.1, ro=1, ch=1)
+                mergeList.append(name + f'foldsMesh{curveNum + 1}')
+            print(mergeList)
+            cmds.polyUnite(mergeList, ch=1, mergeUVSets=1, centerPivot=True, name=name + 'foldsCollider')
+            cmds.select(name + 'foldsCollider')
+            cmds.move(0, 0.7, 0, r=True)
+        #clothmesh
         clothmesh = cmds.polyPlane(w=width, h=width, sx=10, sy=10, ax=[0, 1, 0], cuv=2, ch=1, n= name + 'clothmesh')
         if(clothShape == 1):    
             clothmesh = cmds.polyCircularize(name + 'clothmesh')
@@ -169,4 +113,75 @@ def convertCloth():
         ratio = height/width
         clothmesh = cmds.scale(ratio, 1, 1, relative = True)
         
+        #plane collider
+        collider = cmds.polyPlane(w=width, h=width, sx=10, sy=10, ax=[0, 1, 0], cuv=2, ch=1, n= name + 'collider')
+        
+    clothmesh = cmds.polyPlane(w=width, h=width, sx=10, sy=10, ax=[0, 1, 0], cuv=2, ch=1, n= name + 'clothmesh')
+    if(clothShape == 1):    
+        clothmesh = cmds.polyCircularize(name + 'clothmesh')
+    cmds.select(name + 'clothmesh')
+    ratio = height/width
+    clothmesh = cmds.scale(ratio, 1, 1, relative = True)
+    
+    #location
+    clothLocation = 0, 2, 0
+    if (useTable):
+        clothLocation = cmds.objectCenter(collider)
+        clothLocation[1] += 2
+    elif(useFolds):
+        clothLocation = cmds.objectCenter(name + 'foldsCollider')
+        cmds.select(name + 'collider')
+        cmds.move(clothLocation[0], 0, clothLocation[2], r=True)
+        clothLocation[1] += 2
+    cmds.move(clothLocation[0], clothLocation[1], clothLocation[2], r=True)
+    
+    #add subdivisions to cloth mesh
+    clothSubdivide = 2
+    if (width > 7 or height >  7):
+        clothSubdivide = 3
+    print(clothSubdivide)
+    clothmesh = cmds.polySmooth(name + 'clothmesh', dv = clothSubdivide, mth=0, sl = 2)
+    
+    #convert to nCloth
+    outMesh = cmds.createNode('nRigid', name=name + 'nRigid1')
+    
+    cmds.connectAttr(name + 'collider' + 'Shape.worldMesh[0]', name + 'nRigid1.inputMesh')
+    cmds.createNode('nucleus', name= name + 'nucleus1')
+    cmds.connectAttr(name + 'nRigid1.currentState', name + 'nucleus1.inputPassive[0]')
+    cmds.connectAttr(name + 'nRigid1.startState', name + 'nucleus1.inputPassiveStart[0]')
+    cmds.connectAttr(name + 'nucleus1.startFrame', name + 'nRigid1.startFrame')
+    
+    cmds.createNode('nCloth', name= name + 'nCloth1')
+    cmds.connectAttr(name + 'nucleus1.outputObjects[0]', name + 'nCloth1.nextState')
+    cmds.connectAttr(name + 'clothmeshShape.worldMesh[0]', name + 'nCloth1.inputMesh')
+    cmds.connectAttr(name + 'nucleus1.startFrame', name + 'nCloth1.startFrame')
+    cmds.connectAttr(name + 'nCloth1.startState', name + 'nucleus1.inputActiveStart[0]')
+    cmds.connectAttr(name + 'nCloth1.currentState', name + 'nucleus1.inputActive[0]')
+    outMesh = cmds.createNode('mesh', n = name + 'cloth1')
+    cmds.connectAttr(name + 'nCloth1.outputMesh', name + 'cloth1.inMesh')
+    cmds.setAttr(name + 'nRigid1.thickness', 0.0)
+    cmds.setAttr(name + 'nCloth1.thickness', 0.02)
+    
+    time_node = cmds.createNode('time', n = 'time')
+    cmds.playbackOptions(minTime=1, maxTime=300, animationStartTime=1, animationEndTime=300)
+    cmds.currentTime( query=True )
+    
+    cmds.connectAttr('time.outTime', name + 'nCloth1.currentTime')
+    cmds.connectAttr('time.outTime', name + 'nucleus1.currentTime')
+    cmds.connectAttr('time.outTime', name + 'nRigid1.currentTime')
+    
+    cmds.expression(s=f"{time_node}.outTime = `currentTime -q`;")
+   
+    #nCloth attribute adjustments
+    cmds.setAttr(name + 'nCloth1.lift', 0)
+    cmds.setAttr(name + 'nCloth1.friction', 0.5)   
+    cmds.setAttr(name + 'nCloth1.stickiness', 0.2)
+    cmds.setAttr(name + 'nCloth1.stretchResistance', 120)
+    
+    shader = cmds.shadingNode('aiStandardSurface', asShader = True, n=name + 'shader')
+                
+    cmds.sets(renderable=True, noSurfaceShader= True, empty=True, n= 'aiSurfaceShader' + name + 'SG')
+    cmds.select(outMesh)
+    cmds.hyperShade(assign = 'aiSurfaceShader' + name + 'SG')
+    cmds.connectAttr(name + 'shader.outColor', 'aiSurfaceShader' + name +'SG.surfaceShader', f=True)
         
